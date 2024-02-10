e333afa8d6879f08500e0d5a5bb6d8415ab74248
solflowernm <solflower27@gmail.com>
2024-02-10 16:42:13 -0700

    fixes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express() 
 const port = 3000 
-// const cors = require('cors')
+const cors = require('cors')
 
-// app.use(cors)
+app.use(cors)
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
